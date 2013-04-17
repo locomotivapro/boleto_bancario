3ca3696b8856fd8662a78fbbafcf9b2a2fbc8f4d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-04-17 14:46:54 -0300

    Remove test because we dont have assigns here

--- spec/boleto_bancario/renderers/html_renderer_spec.rb
+++ spec/boleto_bancario/renderers/html_renderer_spec.rb
@@ -6,11 +6,6 @@ describe BoletoBancario::Renderers::HTMLRenderer do
   describe ".render" do
     let(:boleto) { BoletoBancario::Core::Santander.new }
 
-    it "assigns a boleto to be rendered by the template" do
-      subject.render(boleto)
-      assigns(:boleto).should eql boleto
-    end 
-
     context "with no boleto passed" do
       it "returns error message 'Nenhum boleto foi passado'" do
         expect( subject.render ).to eql 'Nenhum boleto foi passado'
