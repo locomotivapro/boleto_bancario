999032517e4fb34e569e468943e2c8a824ced38e
fabiode <eu@fabiode.com.br>
2014-12-02 15:30:22 -0200

    trying to use 1.0 prawn version

--- lib/boleto_bancario/core/santander.rb
+++ lib/boleto_bancario/core/santander.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 require 'prawn'
-require 'prawn/grid'
-require 'prawn/table'
+require 'prawn/layout'
 require 'barby'
 require 'barby/barcode/code_25_interleaved'
 require 'barby/outputter/prawn_outputter'
